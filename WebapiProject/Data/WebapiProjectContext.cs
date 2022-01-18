c8f1b7a846356e614e33a0546601bc1624c824f4
lokes <lokes@LAPTOP-J2B9HTQV>
2022-01-18 12:05:14 +0530

    Add project files.

---
+++ WebapiProject/Data/WebapiProjectContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebapiProject.Model;
+
+namespace WebapiProject.Data
+{
+    public class WebapiProjectContext : DbContext
+    {
+        public WebapiProjectContext (DbContextOptions<WebapiProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebapiProject.Model.Customer> Customer { get; set; }
+    }
+}
